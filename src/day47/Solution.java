add258b4bf18fbf8af400b63478dc1127614c91f
“sweetPototo” <sweetpototo11@gmail.com>
2024-09-29 11:05:34 +0900

    clear

--- src/day47/Solution.java
+++ src/day47/Solution.java
@@ -1,8 +1,8 @@
 package day47;
 
-import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 
 public class Solution {
 	public static void main(String[] args) {
@@ -12,31 +12,51 @@ public class Solution {
 	}
 	
 	public static int solution(int[] number) {
-		HashSet<ArrayList<Integer>> com = new HashSet<>();
+		int answer = 0;
 		int len = number.length;
 		
-		for(int i=0; i<len; ++i) {  //2
-			for(int j=i+1; j<len; ++j) {  //3
+		for(int i=0; i<len-2; ++i) {  //2
+			for(int j=i+1; j<len-1; ++j) {  //3
 				for(int k=j+1; k<len; ++k) { //0
-					ArrayList<Integer> listCom = new ArrayList<>();
 					int sum = number[i] + number[j]+ number[k];
 					System.out.printf("num : %d, %d, %d\n", number[i], number[j], number[k]);
 					if(sum == 0) {
-						System.out.println("sum : " + sum);
-						int group[] = {number[i], number[j], number[k]};
-						for(int g : group) {   //삼충사 리스트에 넣기
-							listCom.add(g);
-						}
-						System.out.println("add data : " + listCom.toString());
-						System.out.println();
-						Collections.sort(listCom);
-						com.add(listCom);
+						++answer;
 					}
 				}
 			}
 		}
-		System.out.println("result : " + com.toString());
-        int answer = com.size();
         return answer;
     }
+	
+	//문제를 잘못 이해함... 같은 정수의 다른 학생이면 하나의 조합 경우라고 생각했었음...
+//	public static int failSolution(int[] number) {
+//		 HashSet<ArrayList<Integer>> com = new HashSet<>();
+//			int len = number.length;
+//			
+//			for(int i=0; i<len; ++i) {
+//				for(int j=i+1; j<len; ++j) {
+//					for(int k=j+1; k<len; ++k) {
+//						ArrayList<Integer> listCom = new ArrayList<>();
+//						int sum = number[i] + number[j]+ number[k];
+//						System.out.printf("num : %d, %d, %d\n", number[i], number[j], number[k]);
+//						if(sum == 0) {
+//							System.out.println("sum : " + sum);
+//							int group[] = {number[i], number[j], number[k]};
+//							for(int g : group) {
+//								listCom.add(g);
+//							}
+//							System.out.println("add data : " + listCom.toString());
+//							System.out.println();
+//							Collections.sort(listCom);
+//							com.add(listCom);
+//						}
+//					}
+//				}
+//			}
+//			System.out.println("result : " + com.toString());
+//	        int answer = com.size();
+//	        return answer;
+//    }
+	
 }
