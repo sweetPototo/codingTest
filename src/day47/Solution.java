5b66ceaf0bdca27872511e7c598bc6be6efb96ea
sweetPototo <sweetpototo@gmail.com>
2024-09-29 09:57:59 +0900

    fix same case

--- src/day47/Solution.java
+++ src/day47/Solution.java
@@ -2,12 +2,13 @@ package day47;
 
 import java.util.HashSet;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Solution {
 	public static void main(String[] args) {
-		int[] number = new int[] {-2, 3, 0, 2, -5};
+		int[] number = new int[] {-1, -1, 2, 2, 0, 0};
 		int result = solution(number);
-		System.out.println(result);
+		System.out.println("result : " + result);
 	}
 	
 	public static int solution(int[] number) {
@@ -15,28 +16,26 @@ public class Solution {
 		int len = number.length;
 		
 		for(int i=0; i<len; ++i) {  //2
-			
 			for(int j=i+1; j<len; ++j) {  //3
 				for(int k=j+1; k<len; ++k) { //0
-					int group[] = {number[i], number[j], number[k]};
 					ArrayList<Integer> listCom = new ArrayList<>();
-					for(int g : group) {   //삼충사 리스트에 넣기
-						listCom.add(g);
-					}
-					int sum = 0;
-					System.out.println(listCom.size());
-					for(Integer num : listCom) {
-						System.out.println("num : " + num);
-						sum += num;
-					}
-					System.out.println();
+					int sum = number[i] + number[j]+ number[k];
+					System.out.printf("num : %d, %d, %d\n", number[i], number[j], number[k]);
 					if(sum == 0) {
 						System.out.println("sum : " + sum);
+						int group[] = {number[i], number[j], number[k]};
+						for(int g : group) {   //삼충사 리스트에 넣기
+							listCom.add(g);
+						}
+						System.out.println("add data : " + listCom.toString());
+						System.out.println();
+						Collections.sort(listCom);
 						com.add(listCom);
 					}
 				}
 			}
 		}
+		System.out.println("result : " + com.toString());
         int answer = com.size();
         return answer;
     }
