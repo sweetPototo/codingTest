cf255f2627c7629390eb230060e3d6a6c7538776
sweetPototo <sweetpototo11@gmail.com>
2024-10-28 23:38:52 +0900

    day 51

---
+++ src/day51/Solution.java
@@ -0,0 +1,14 @@
+package day51;
+
+public class Solution {
+	
+	public static int solution(String s) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("one4seveneight");
+
+	}
+
+}
