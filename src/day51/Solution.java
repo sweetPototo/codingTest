bf8c634fb9318871f20d8238f2a0d1abf2770d66
sweetPototo <sweetpototo11@gmail.com>
2024-10-29 01:23:40 +0900

    51 day

--- src/day51/Solution.java
+++ src/day51/Solution.java
@@ -1,9 +1,28 @@
 package day51;
 
 public class Solution {
+	public static int changeNumber(String s) {
+		//48~57
+		String[] number = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+		StringBuilder result = new StringBuilder();
+		StringBuilder numberString = new StringBuilder();
+		for(int i = 0; i<s.length(); ++i) {
+			char alphabet = s.charAt(i);
+			if(alphabet >= 48 || alphabet <= 57) {
+				result.append(s.charAt(i));
+			}else {
+				
+			}
+		}
+	}
 	
 	public static int solution(String s) {
-		return null;
+		try {
+			int result = Integer.parseInt(s);
+			return result;
+		}catch (NumberFormatException e) {
+			return changeNumber(s);
+		}
 	}
 
 	public static void main(String[] args) {
