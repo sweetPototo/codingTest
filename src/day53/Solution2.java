b588973297624703292c03538a373ee347448414
sweetPototo <sweetpototo11@gmail.com>
2024-11-08 23:43:21 +0900

    day54

--- src/day53/Solution2.java
+++ src/day53/Solution2.java
@@ -1,13 +1,21 @@
 package day53;
 
 import java.util.Collections;
+import java.util.Comparator;
 
-public class Solution2 {
+public class Solution2 implements Comparator<Integer>{
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Collections.sort(null);
 
+	}
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
