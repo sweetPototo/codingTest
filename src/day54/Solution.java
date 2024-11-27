309dcb163df4efc7b192c60a9a28b89974f78aee
sweetPototo <sweetpototo11@gmail.com>
2024-11-27 23:59:10 +0900

    commit

--- src/day54/Solution.java
+++ src/day54/Solution.java
@@ -1,9 +1,26 @@
 package day54;
 
 public class Solution {
+	
+	public static String solution(int[] food) {
+		int count = 1;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<food.length; ++i) {
+        	if(food[i] == 1) {
+        		continue;
+        	}else {
+        		int dish = food[i]/2;
+        		for(int j=0; j<dish; ++j) {
+        			sb.append(count);
+        		}
+        		count++;
+        	}
+        }
+        return null;
+    }
 
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("solution : " + solution(new int[] {1, 3, 4, 6}));
 
 	}
 
