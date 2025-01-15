5ebc52e4bb34233beb796c1328d3ed356314e613
jiyoung <sweetpototo11@gmail.com>
2025-01-15 14:05:49 +0900

    add t_bfile

--- src/day53/Solution2.java
+++ src/day53/Solution2.java
@@ -7,10 +7,8 @@ import java.util.List;
 
 public class Solution2 {
 	public static String[] solution(String[] strings, int n) {
-		List<String> stringList = new ArrayList<>();
-		for(String s : strings) {
-			stringList.add(s);
-		}
+		List<String> stringList = new ArrayList<>(Arrays.asList(strings));
+		
 		//람다로 구현
 		stringList.sort((o1, o2) -> {
 			if(o1.charAt(n) > o2.charAt(n)) { //n의 아스키 코드값이 더 크면
@@ -35,31 +33,33 @@ public class Solution2 {
 		});
 		
 		//익명클래스로 구현
-//		stringList.sort(new Comparator<String>() {
-//			@Override
-//			public int compare(String o1, String o2) {
-//				if(o1.charAt(n) > o2.charAt(n)) { //n의 아스키 코드값이 더 크면
-//					return 1;
-//				}else if(o1.charAt(n) == o2.charAt(n)) {  //n의 아스키 코드값이 같으면
-//					int lenght = Math.min(o1.length(), o2.length());
-//					for(int i=0; i<lenght; ++i) {
-//						if(o1.charAt(i) > o2.charAt(i)) {
-//							return 1;
-//						}else if(o1.charAt(i) == o2.charAt(i)) {
-//							continue;
-//						}else {
-//							return -1;
-//						}
-//					}
-//				}else {  //n의 아스키값이 더 작으면
-//					return -1;
-//				}
-//				
-//				return 0; //두 문자열이 완벽하게 일치
-//			}
-//		});
+		stringList.sort(new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.charAt(n) > o2.charAt(n)) { //n의 아스키 코드값이 더 크면
+					return 1;
+				}else if(o1.charAt(n) == o2.charAt(n)) {  //n의 아스키 코드값이 같으면
+					int lenght = Math.min(o1.length(), o2.length());
+					for(int i=0; i<lenght; ++i) {
+						if(o1.charAt(i) > o2.charAt(i)) {
+							return 1;
+						}else if(o1.charAt(i) == o2.charAt(i)) {
+							continue;
+						}else {
+							return -1;
+						}
+					}
+				}else {  //n의 아스키값이 더 작으면
+					return -1;
+				}
+				
+				return 0; //두 문자열이 완벽하게 일치
+			}
+		});
 		return stringList.toArray(new String[stringList.size()]);
 	}
+	
+	
 	public static void main(String[] args) {
 		Arrays.stream(solution(new String[] {"abce", "cdx", "abcd"}, 2)).forEach(System.out::println);
 	}
--- src/day54/Solution.java
+++ src/day54/Solution.java
@@ -1,22 +1,30 @@
 package day54;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
 	
 	public static String solution(int[] food) {
-		int count = 1;
-        StringBuilder sb = new StringBuilder();
+		int count = 0;
+        StringBuilder front = new StringBuilder();
+        StringBuilder behind = new StringBuilder();
         for(int i=0; i<food.length; ++i) {
         	if(food[i] == 1) {
+        		count++;
         		continue;
         	}else {
         		int dish = food[i]/2;
         		for(int j=0; j<dish; ++j) {
-        			sb.append(count);
+        			front.append(count);
+        			behind.append(count);
         		}
         		count++;
         	}
         }
-        return null;
+        front.append("0").append(behind.reverse());
+        return front.toString();
     }
 
 	public static void main(String[] args) {
